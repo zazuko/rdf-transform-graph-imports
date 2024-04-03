10354f69e45e157755d75c7d88af34810fc7eb45
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2024-04-03 16:22:25 +0200

    feat: allow no fs factory

--- lib/env.ts
+++ lib/env.ts
@@ -4,5 +4,5 @@ import type FsUtilsFactory from '@zazuko/rdf-utils-fs/Factory.js'
 import type { FetchFactory } from '@rdfjs/fetch-lite/Factory.js'
 import type ClownfaceFactory from 'clownface/Factory.js'
 
-type Environment = RdfjsEnvironment<NamespaceFactory | FsUtilsFactory | FetchFactory | ClownfaceFactory>
+type Environment = RdfjsEnvironment<NamespaceFactory | FsUtilsFactory | FetchFactory | ClownfaceFactory> | RdfjsEnvironment<NamespaceFactory | FetchFactory | ClownfaceFactory>
 export default Environment
